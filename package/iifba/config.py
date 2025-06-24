7e14944a0431bac57554ad82bbaa8104093fac7a
RSiegel03 <ryancgl26@gmail.com>
2025-06-24 10:43:10 -0400

    Update config.py

--- package/iifba/config.py
+++ package/iifba/config.py
@@ -3,8 +3,8 @@ import cobra as cb
 GROWTH_MIN_OBJ = 0.01
 
 def load_example_model():
-    example_ecoli = cb.io.load_matlab_model("AGORA2_Models/Escherichia_coli_str_K_12_substr_MG1655.mat")
-    
+	example_ecoli = cb.io.load_matlab_model("AGORA2_Models/Escherichia_coli_str_K_12_substr_MG1655.mat")
+	
 	#ecoli example glucose minimal media
 	min_med_ids_ex = ['EX_glc_D(e)','EX_so4(e)','EX_nh4(e)','EX_no3(e)','EX_pi(e)','EX_cys_L(e)',
 				'EX_mn2(e)','EX_cl(e)','EX_ca2(e)','EX_mg2(e)','EX_cu2(e)','EX_cobalt2(e)','EX_fe2(e)','EX_fe3(e)','EX_zn2(e)','EX_k(e)']
@@ -12,5 +12,5 @@ def load_example_model():
 	min_med_fluxes_ex = [-10,-100,-100,-100,-100,-100,
 						-100,-100,-100,-100,-100,-100,-100,-100,-100,-100]
 	ecoli_media_example = dict(zip(min_med_ids_ex, min_med_fluxes_ex))
-    
-    return example_ecoli, ecoli_media_example
+	
+	return example_ecoli, ecoli_media_example
