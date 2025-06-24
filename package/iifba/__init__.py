9e5c69228f14fe4ac3fca3fcbb99b5f4069c6476
RSiegel03 <ryancgl26@gmail.com>
2025-06-24 10:49:37 -0400

    adjustments to package struct

--- package/iifba/__init__.py
+++ package/iifba/__init__.py
@@ -1,8 +1,10 @@
 from .analysis import iipfba, iisampling, aux_analysis
 from .utils import iifba_vis
+from .config import load_example_model
 
 __all__ = ["iipfba",
            "iisampling",
-           "aux_analysis"
-           "iifba_vis"
+           "aux_analysis",
+           "iifba_vis",
+           "load_example_model"
            ]
